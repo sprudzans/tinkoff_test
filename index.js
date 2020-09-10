d3ca635a03499f90118ab3162a19b0fd1582758e
sprudzans <a.sprudzans@mail.com>
2020-09-10 13:49:56 +0300

    class Cart

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
 'use strict'
 
-let catalog = document.querySelector('#catalog'),
-    cartCall = document.querySelector('#cartCall'),
-    cart = document.querySelector('#cart'),
-    cartValue = document.querySelector('#cartValue'),
-    cartQuantity = document.querySelector('#cartQuantity'),
-    data = [];
+class Cart {
+    constructor(){
+        this.data = [];
+        this.catalogJSON = {
+                "products": [
+                    {
+                        "id": 1,
+                        "name": "Декоративная маска \"Rules\"",
+                        "image": "https://mamcupy.com/upload/resize_cache/iblock/e66/610_406_140cd750bba9870f18aada2478b24840a/e6699c6bcd9d42491ad6d37de20aea66.jpg",
+                        "price": 600,
+                        "description": "Маски выкраиваются из большого полотна, запечатанного принтом, поэтому рисунок на маске может отличаться от изделия к изделию."
+                    },
+                    {
+                        "id": 2,
+                        "name": "Декоративная маска \"RBL\"",
+                        "image": "https://mamcupy.com/upload/resize_cache/iblock/b8e/610_406_140cd750bba9870f18aada2478b24840a/b8ef6fc8bbc94b58320ba803a9016699.jpg",
+                        "price": 600,
+                        "description": "Маски выкраиваются из большого полотна, запечатанного принтом, поэтому рисунок на маске может отличаться от изделия к изделию."
+                    },
+                    {
+                        "id": 3,
+                        "name": "Декоративная маска \"Дома\"",
+                        "image": "https://mamcupy.com/upload/resize_cache/iblock/db3/610_406_140cd750bba9870f18aada2478b24840a/db3de205df3199ef36cc9131d19a145c.jpg",
+                        "price": 600,
+                        "description": "Маски выкраиваются из большого полотна, запечатанного принтом, поэтому рисунок на маске может отличаться от изделия к изделию."
+                    }
+                ]
+            };
+    }
 
-let catalogJSON = {
-    "products": [
-        {
-            "id": 1,
-            "name": "Декоративная маска \"Rules\"",
-            "image": "https://mamcupy.com/upload/resize_cache/iblock/e66/610_406_140cd750bba9870f18aada2478b24840a/e6699c6bcd9d42491ad6d37de20aea66.jpg",
-            "price": 600,
-            "description": "Маски выкраиваются из большого полотна, запечатанного принтом, поэтому рисунок на маске может отличаться от изделия к изделию."
-        },
-        {
-            "id": 2,
-            "name": "Декоративная маска \"RBL\"",
-            "image": "https://mamcupy.com/upload/resize_cache/iblock/b8e/610_406_140cd750bba9870f18aada2478b24840a/b8ef6fc8bbc94b58320ba803a9016699.jpg",
-            "price": 600,
-            "description": "Маски выкраиваются из большого полотна, запечатанного принтом, поэтому рисунок на маске может отличаться от изделия к изделию."
-        },
-        {
-            "id": 3,
-            "name": "Декоративная маска \"Дома\"",
-            "image": "https://mamcupy.com/upload/resize_cache/iblock/db3/610_406_140cd750bba9870f18aada2478b24840a/db3de205df3199ef36cc9131d19a145c.jpg",
-            "price": 600,
-            "description": "Маски выкраиваются из большого полотна, запечатанного принтом, поэтому рисунок на маске может отличаться от изделия к изделию."
-        }
-    ]
-}
+    init = function () {
+        fetch("./data.json")
+            .then(response => response.json())
+            .then((json) => {
+                this.data = json
+                this.catalogLoad(this.data)
+            })
+            .catch((e) => {
+                console.log(e)
+                this.data = this.catalogJSON
+                this.catalogLoad(this.data)
+            })
+    }
 
-fetch("./data.json")
-    .then(response => response.json())
-    .then((json) => {
-        data = json
-        catalogLoad(data)
-    })
-    .catch((e) => {
-        console.log(e)
-        data = catalogJSON
-        catalogLoad(data)
-    })
-
-
-function catalogLoad(data) {
-    data["products"].forEach(element => {
-        catalog.innerHTML += `
+    catalogLoad(data) {
+        console.log(this);
+        this.data.products.forEach(element => {
+            catalog.innerHTML += `
             <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
                     <img class="card-img-top" src="${element.image}" alt="${element.name}" >
@@ -58,20 +57,19 @@ function catalogLoad(data) {
                         <p class="card-text">${element.description}</p>
                         <div class="d-flex justify-content-end"> 
                             <div class="btn-group"> 
-                                <button type="button" class="btn btn-success" onclick="addCart(${element.id})">Добавить в корзину</button>
+                                <button type="button" class="btn btn-success" onclick="shop.addCart(${element.id})">Добавить в корзину</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        `;
-    });
-}
+            </div>`
+        });
+    }
 
-function addCart(id) {
-    let item = data.products.find(item => item.id == id);
+    addCart(id) {
+        let item = this.data.products.find(item => item.id == id);
 
-    cart.innerHTML += `
+        cart.innerHTML += `
         <div class="row" id="cartItem" data-item="${item.id}">
             <div class="col-4">
                 <img class="img-responsive" src="${item.image}" alt="${name}" width="120">
@@ -81,63 +79,67 @@ function addCart(id) {
                 <h6 class="text-muted"><span id="price">${item.price}</span> р.</h6>
             </div>
             <div class="col-2">
-                <input id="quantity" type="number" size="10" min="1" value="1" onchange="reloadCart();">
+                <input id="quantity" type="number" size="10" min="1" value="1" onchange="shop.reloadCart();">
             </div>
         </div>
     `;
-    cartCall.removeAttribute('disabled');
-    reloadCart();
-}
-
-function reloadCart() {
-    let sum = 0;
-    for (let item of cart.children) {
-        sum += parseInt(item.querySelector('#price').textContent) * item.querySelector('#quantity').value;
+        cartCall.removeAttribute('disabled');
+        this.reloadCart();
     }
-    cartValue.value = sum;
-    if (cart.children.length !== 0){
-        cartQuantity.textContent = cart.children.length;
-    } else {
-        cartQuantity.textContent = ''
+
+    reloadCart() {
+        let sum = 0;
+        for (let item of cart.children) {
+            sum += parseInt(item.querySelector('#price').textContent) * item.querySelector('#quantity').value;
+        }
+        cartValue.value = sum;
+        if (cart.children.length !== 0){
+            cartQuantity.textContent = cart.children.length;
+        } else {
+            cartQuantity.textContent = ''
+        }
     }
-}
 
-function clearCart() {
-    cart.innerHTML = '';
-    reloadCart();
-    cartCall.setAttribute("disabled", "disabled");
-}
+    clearCart() {
+        cart.innerHTML = '';
+        this.reloadCart();
+        cartCall.setAttribute("disabled", "disabled");
+    }
 
-function buyCart() {
-    let arr = [];
-    for (let item of cart.children) {
-        arr.push({
-            "Name": item.querySelector('h5').textContent,
-            "Price": parseInt(item.querySelector('#price').textContent) * 100,
-            "Quantity": parseInt(item.querySelector('#quantity').value),
-            "Amount": parseInt(item.querySelector('#price').textContent) * 100 * parseInt(item.querySelector('#quantity').value),
-            "Tax": "vat20",
+    buyCart() {
+        let arr = [];
+        for (let item of cart.children) {
+            arr.push({
+                "Name": item.querySelector('h5').textContent,
+                "Price": parseInt(item.querySelector('#price').textContent) * 100,
+                "Quantity": parseInt(item.querySelector('#quantity').value),
+                "Amount": parseInt(item.querySelector('#price').textContent) * 100 * parseInt(item.querySelector('#quantity').value),
+                "Tax": "vat20",
+            })
+        }
+
+        fetch('https://securepay.tinkoff.ru/v2/Init', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "TerminalKey": "TinkoffBankTest",
+                "Amount": cartValue.value * 100,
+                "OrderId": "120423",
+                "Description": "Buying masks",
+                "Receipt": {
+                    "Email": "a@test.ru",
+                    "Taxation": "osn",
+                    "Items": arr
+                }
+            })
         })
+            .then(response => response.json())
+            .then(json => window.location.replace(json.PaymentURL))
+            .catch(e => console.log(e));
     }
-
-    fetch('https://securepay.tinkoff.ru/v2/Init', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "TerminalKey": "TinkoffBankTest",
-            "Amount": cartValue.value * 100,
-            "OrderId": "120423",
-            "Description": "Buying masks",
-            "Receipt": {
-                "Email": "a@test.ru",
-                "Taxation": "osn",
-                "Items": arr
-            }
-        })
-    })
-        .then(response => response.json())
-        .then(json => window.location.replace(json.PaymentURL))
-        .catch(e => console.log(e));
 }
+
+let shop = new Cart;
+shop.init()
